b8389ae87da32bbfc69258729d1a5a4de8f22226
davidtav <david.mclaurel@outlook.com.br>
2026-02-21 19:44:00 -0300

    feat: uso do java.util.Scanner para saida no terminal e printf na soma de 2 numeros

---
+++ Addition.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Addition {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num1, num2, sum;
+        System.out.print("digite o primeiro número: ");
+        num1 = input.nextInt();
+        System.out.print("digite o segundo número: ");
+        num2 = input.nextInt();
+        sum = num1 + num2;
+        System.out.printf("a soma dos números é: %d\n", sum);
+    }
+}
